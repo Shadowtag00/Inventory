5d7d9cc3fdb56a9b18c24adac483fef900307d82
Shadowtag00 <nealmandaliya@gmail.com>
2022-12-03 17:52:37 -0600

    New commit

--- shop/main/Main.java
+++ shop/main/Main.java
@@ -4,10 +4,13 @@ import shop.ui.UIFactory;
 import shop.ui.UI;
 import shop.data.Data;
 
+import javax.swing.*;
+
 public class Main {
   private Main() {}
   public static void main(String[] args) {
     UI ui;
+    JOptionPane.showMessageDialog(null, "Loading...");
     if (Math.random()<=0.5) {
       ui = UIFactory.TextUI();
     } else {
